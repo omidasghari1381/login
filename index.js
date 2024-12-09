72c20223e4e14b0c1e7cbcf5ee3ab8c3752768e1
unknown <1381omidasghari@gmail.com>
2024-12-10 03:13:45 +0330

    added routs

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
+
 app.listen(5000, () => {
-    console.log("listening on port 5000")
-})
+  console.log("listening on port 5000");
+});
+
+const loggingRouter = require("./routes/logging");
+app.use('/logging', loggingRouter);

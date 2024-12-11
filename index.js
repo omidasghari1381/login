4aef84138f39583b542ac27dbda7688b6fdf3a88
unknown <1381omidasghari@gmail.com>
2024-12-11 18:53:46 +0330

    added mongodb to file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  console.log("db connected")
+}
 
 app.listen(5000, () => {
   console.log("listening on port 5000");
 });
-
-const loggingRouter = require("./routes/logging");
-app.use('/logging', loggingRouter);

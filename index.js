26fc234aee0ea04925c87dc92dc7444d9e9d62e0
unknown <1381omidasghari@gmail.com>
2024-12-12 02:18:53 +0330

    added .env

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require("express");
+import express from "express";
 const app = express();
-const mongoose = require("mongoose");
+
+import { connect } from "mongoose";
+
+import 'dotenv/config'
 
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await connect(`mongodb://127.0.0.1:27017/${process.env.DB_Name}`);
   console.log("db connected")
 }
 
-app.listen(5000, () => {
-  console.log("listening on port 5000");
+import UserName from './models/auth';
+const mamad = new UserName({ username : 'mamad' ,email : '123465mamad' , password : '12345' , phone : 123456})
+await mamad.save();
+
+const users = await UserName.find();
+console.log(users);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`listening on port ${process.env.SERVER_PORT}`);
 });
